1b4ce0a909758cfd045fb7eb9a3b2e95ea675c38
Nick Miller <n@nickmiller.io>
2021-04-25 14:24:32 -0700

    refactor, add JSDocs

--- ynab.js
+++ ynab.js
@@ -10,8 +10,6 @@ const {
 
 const SAFE_WITHDRAWL_RATE = 0.04;
 
-
-
 const ynabAPI = new ynab.API(accessToken);
 
 const formatter = new Intl.NumberFormat('en-US', {
@@ -19,12 +17,18 @@ const formatter = new Intl.NumberFormat('en-US', {
   currency: 'USD',
 });
 
+/** Retrieves the budget ID based on the budget name */
 const getBudgetId = async () => {
   const budgetsResponse = await ynabAPI.budgets.getBudgets();
   const budgets = budgetsResponse.data.budgets;
   return budgets.filter((budget => budget.name === budgetName))[0].id;
 };
 
+/*
+-------- Retirement Income Functions -----------
+*/
+
+/** Returns account information based on the names of the retirement accounts in config.js */
 const getRetirementAccounts = async (budgetId) => {
   const accountData = await ynabAPI.accounts.getAccounts(budgetId);
   const accounts = accountData.data.accounts;
@@ -35,6 +39,7 @@ const getRetirementAccounts = async (budgetId) => {
   return retirementAccounts;
 };
 
+/** Return an array balances for an array of accounts */
 const getAccountBalances = (accounts) => {
   const balances = [];
   for (let account of accounts) {
@@ -46,9 +51,12 @@ const getAccountBalances = (accounts) => {
   return adjustedBalances;
 }
 
+/** Helper function to format the retirement income output */
 const outputIncomeReport = (retirementValues) => {
   const { totalBalance, annualIncome, monthlyIncome } = retirementValues;
 
+  console.log();
+  console.log('------- Retirement Income -------');
   console.log();
   console.log(`Total retirement funds: ${totalBalance}`);
   console.log(`Annual retirement income: ${annualIncome}`);
@@ -56,6 +64,7 @@ const outputIncomeReport = (retirementValues) => {
   console.log();
 };
 
+/** Returns the total balance from retirement accounts and projected annual and monthly income */
 const calculateRetirementValue = async (budgetId) => {
   const retirementAccounts = await getRetirementAccounts(budgetId);
   const retirementAccountBalances = await getAccountBalances(retirementAccounts);
@@ -64,6 +73,7 @@ const calculateRetirementValue = async (budgetId) => {
   return formatRetirementValues(totalRetirementValue);
 };
 
+/** Formats balances and income into dollar amounts based on safe withdrawl rate in config.js  */
 const formatRetirementValues = (totalValue) => {
   const totalBalance = formatter.format(totalValue);
   const annualIncome = formatter.format(totalValue * safeWithdrawlRate);
@@ -71,6 +81,7 @@ const formatRetirementValues = (totalValue) => {
   return { totalBalance, annualIncome, monthlyIncome };
 };
 
+/** Returns categroy group information for groups in config.js */
 const filterCategoryGroupData = (allCategoryGroups) => {
   const categoryNames = [];
   categoryGroups.forEach(category => categoryNames.push(category.name));
@@ -79,11 +90,10 @@ const filterCategoryGroupData = (allCategoryGroups) => {
   });
 }
 
-const calculateExpenditureValues = async (budgetId) => {
-  const rawCategoryData = await ynabAPI.categories.getCategories(budgetId);
-  const allCategoryGroups = rawCategoryData.data.category_groups;
-  const filteredGroupRawData = filterCategoryGroupData(allCategoryGroups);
+/** Calculates expenditure totals based on category group value in config.js */
+const extractExpendituresByType = (filteredGroupRawData) => {
   const selectedGroupData = [];
+
   categoryGroups.forEach(group => {
     const groupData = { name: group.name, type: group.type };
     const categoryGroupData = filteredGroupRawData.find(groupObject => groupObject.name === group.name);
@@ -98,23 +108,42 @@ const calculateExpenditureValues = async (budgetId) => {
     selectedGroupData.push(groupData)
   })
 
+  return selectedGroupData;
+};
+
+/** Calcalates essential and total expenditure amounts */
+const formatExpenditureOutput = (selectedGroupData) => {
   let essentialExp = selectedGroupData.filter(group => group.type === 'essential').reduce((a, b) => ({ value: a.value + b.value })).value;
   let totalExp = selectedGroupData.reduce((a, b) => ({ value: a.value + b.value })).value;
   essentialExp = formatter.format(essentialExp);
   totalExp = formatter.format(totalExp);
 
-  return { essentialExp, totalExp };
+  return { essentialExp, totalExp }
+}
+
+/** Retrieves category group information and returns expenditure amount based on group value and type */
+const calculateExpenditureValues = async (budgetId) => {
+  const rawCategoryData = await ynabAPI.categories.getCategories(budgetId);
+  const allCategoryGroups = rawCategoryData.data.category_groups;
+  const filteredGroupRawData = filterCategoryGroupData(allCategoryGroups);
+  selectedGroupData = extractExpendituresByType(filteredGroupRawData);
+
+  return formatExpenditureOutput(selectedGroupData);
 };
 
+/** Helper function to format the expenses output */
 const outputExpenditureReport = (expenses) => {
   const { essentialExp, totalExp } = expenses;
 
+  console.log()
+  console.log('----------- Expenses ------------')
   console.log();
   console.log(`Essential expenses this month: ${essentialExp}`);
   console.log(`Total expenses this month: ${totalExp}`);
   console.log();
 };
 
+/** Main function */
 (async function() {
   const budgetId = await getBudgetId();
   const retirementValues = await calculateRetirementValue(budgetId); 
